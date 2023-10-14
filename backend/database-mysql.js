95f10df70794ad8cc8c30679b1c670e462fbad17
Lucas Batista de Oliveira <lucas.lucas2004.30@gmail.com>
2023-10-14 00:26:23 -0300

    rotas criadas

--- backend/database-mysql.js
+++ backend/database-mysql.js
@@ -11,8 +11,8 @@ module.exports = {
 
                 acept(results)
             })
-        }
-    )},
+        })
+    },
 
     loginAuth: (email, password) => {
         return new Promise ((acept, rejected) => {
@@ -20,5 +20,78 @@ module.exports = {
                 { error ? rejected(error) : acept(results)}
             })
         }) 
-    }
+    },
+
+    createUser: (username, email, dateBirth, password) => {
+        return new Promise ((acept, rejected) => {
+            db.query('INSERT INTO usuario (nome, senha, email, nascimento) VALUES (?, ?, ?, ?)', [ username, email, dateBirth, password ], (error, results) => {
+                { error ? rejected(error) : acept(results) }
+            })
+        })
+    },
+    
+    ranking: () => {
+        return new Promise ((acept, rejected) => {
+            db.query(`SELECT usuario_id, nome, SUM(pontuacao) as soma
+                        FROM  usuarioquiz
+                            INNER JOIN usuario ON usuario.id = usuarioquiz.usuario_id
+                        GROUP BY usuario_id
+                        ORDER BY soma DESC
+                        LIMIT 10`, (error, results) => {
+                            { error ? rejected(error) : acept(results) }
+                        })
+        })
+    },
+
+    alterUser: (id, username, email, dateBirth, password) => {
+        return new Promise ((acept, rejected) => {
+            db.query(`UPDATE usuario
+                        SET nome = ?, email = ?, senha = ?, nascimento = ? 
+                        WHERE id = ?     
+                    `, [ username, email, password, dateBirth, id], (error, results) => {
+                        { error ? rejected(error) : acept(results)}
+                    })
+        })
+    },
+
+    historic: (id) => {
+        return new Promise ((acept, rejected) => {
+            db.query(`SELECT * 
+                        FROM usuarioquiz
+                        INNER JOIN quiz ON usuarioquiz.quiz_id = quiz.id
+                        WHERE usuarioquiz.usuario_id = ?
+                    `, [ id ], (error, results) => {
+                        { error ? rejected(error) : acept(results) }
+                    })
+        })
+    },
+    
+    createQuiz: (id, materia, nivel) => {
+        return new Promise ((acept, rejected) => {
+            db.query(`INSERT INTO quiz (id, materia, nivel) VALUES (?, ?, ?)`, [id, materia, nivel ], (error, results) => {
+                { error ? rejected(error) : acept(results) }
+            })
+        })
+    },
+
+    createQuestion: (idQuestion, idQuiz, enunciado, dica, comentario) => {
+        return new Promise ((acept, rejected) => {
+            db.query(`INSERT INTO pergunta (id, quiz_id, enunciado, dica, comentario) 
+                        VALUES (?, ?, ?, ?, ?)
+                    `, [ idQuestion, idQuiz, enunciado, dica, comentario ], (error, results) => {
+                        { error ? rejected(error) : acept(results) }
+            })
+        })
+    }, 
+
+    createAlternative: (idQuestion, descricao, pontuacao) => {
+        return new Promise ((acept, rejected) => {
+            db.query(`INSERT INTO alternativa (pergunta_id, alternativas, pontuacao) 
+                        VALUES (?, ?, ?)
+                    `, [ idQuestion, descricao, pontuacao ], (error, results) => {
+                        { error ? rejected(error) : acept(results) }
+            })
+        })
+    }, 
+    
 }
--- backend/routes.js
+++ backend/routes.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const database = require('./database-mysql')
+const random = require('node:crypto')
+
 
 const router = express.Router()
+
 
 router.get('/quizzes', async (req, res) => {
     const quizzes = await database.searchAll()
 
-    console.log(quizzes)
-
     res.status(200).json(quizzes)
 })
 
+// Tela de login 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body
 
@@ -21,6 +23,69 @@ router.post('/login', async (req, res) => {
     }
 
     res.json({ message: "Parametros recebidos", user: email})
+})
+
+// Tela de cadastro
+router.post('/cadastro', async (req, res) => {
+    const { username, email, dateBirth, password } = req.body                                     
+
+    console.log(username, email, dateBirth, password)
+
+    await database.createUser(username, email, dateBirth, password)
+
+    res.status(201).json({ message: "Usuário criado" })
+})
+
+// Ranking
+router.get('/ranking', async (req, res) => {
+    const ranking = await database.ranking() 
+
+    res.status(200).json({ ranking })
+})
+
+// Alterar usuário
+router.put('/alterar', async (req, res) => {
+    const { id, username, email, dateBirth, password } = req.body
+
+    await database.alterUser(id, username, email, dateBirth, password)
+
+    res.status(200).json({ message: "Usuário alterado!" })
+})
+
+// Historico
+router.get('/historico', async (req, res) => {
+    const id = req.body
+
+    const historico = await database.historic(id)
+
+    res.status(200).json({ historico })
+})
+
+// Criar quiz
+router.post('/criarquiz', async (req, res) => {
+    const { quiz } = req.body
+
+    // console.log(quiz.pergunta[0].enunciado)
+
+    const idQuiz = random.randomUUID()
+    
+    await database.createQuiz(idQuiz, quiz.materia, quiz.nivel)
+
+    for (var cont = 0; cont < quiz.pergunta.length; cont++) {
+        
+        const { enunciado, alternativas, dica, comentario } = quiz.pergunta[cont]
+        const idQuestion = random.randomUUID()
+
+        await database.createQuestion(idQuestion, idQuiz, enunciado, dica, comentario)
+
+        for (var ind = 0; ind < alternativas.length; ind++) {
+            const { descricao, pontuacao } = alternativas[ind]
+
+            await database.createAlternative(idQuestion, descricao, pontuacao)
+        }
+    }
+    
+    res.status(200).json({ message: "Quiz criado!" })
 })
 
 module.exports = router

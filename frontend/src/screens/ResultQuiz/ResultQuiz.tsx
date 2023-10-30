be3640aa7bb82970529f274e3f4dcb7c8dafdbfb
Rafael Campos <rafael.campos.m@hotmail.com>
2023-10-30 16:39:13 -0300

    Creating result screen: progress 80%

--- frontend/src/screens/ResultQuiz/ResultQuiz.tsx
+++ frontend/src/screens/ResultQuiz/ResultQuiz.tsx
@@ -5,32 +5,47 @@ import './style.css'
 export default function ResultQuiz() {
     return (
         <div className="ResultQuiz">
-
+            
             <HeaderUser />
 
             <main>
+
+               
+
+
+                <div className="ResultQuiz">
+
                 <h1>Trigonometria</h1>
-    
 
-            <div className="ResultQuiz">
-
-                <div className="container">
-                    <img className="imagem" src="../../src/assets/result.jpg" alt="Img de resultados" />
                     <div className="container">
-                        <h3>Básico </h3>
-                        <h3>Total:10 </h3>
+                        
+                        <img className="imagem" src="../../src/assets/result.jpg" alt="Img de resultados" />
+                        
+                        <div className="box" >
+                            <h3> <strong> Básico</strong> </h3>
+                            <h3>Total:10 </h3>
+                        </div>
+                        <div className="boxNivel" >
+                            <h3 className="nivel" > Nível:Fácil</h3>
+                            <h3>Pontos:300</h3>
+
+                        </div>
+                        
+                     
+
                     </div>
-                
-                </div>
-                <div>
-                    <h3>Nível:Fácil</h3>
-                    <h3>Pontos:300</h3>
                     
+                    
+                    <hr  />
+                    <div className="container">
+                        <h3>Acertos: 9/10</h3>
+                        <h3><strong>Pontuação</strong></h3>
+                        <h3><strong>270</strong></h3>
+
+                     </div>
+
                 </div>
-                <div className="line">_____________</div>
-            
-            </div>
-           
+                
             </main>
         </div>
 
